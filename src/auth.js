864855dd51fa8f41ba2f15b61bc5b8f7b977fd5c
Dr Jose Mendoza <6201999+jrmst102@users.noreply.github.com>
2026-03-14 19:38:50 +0000

    Users

--- src/auth.js
+++ src/auth.js
@@ -6,6 +6,7 @@ const USERS = [
   { email: 'jose.mendoza@nyu.edu', password: 'Admin123!', name: 'Admin', role: 'admin' },
   { email: 'demo@nyu.edu', password: 'demo1234', name: 'Demo User', role: 'user' },
   { email: 'hc2379@nyu.edu', password: 'Huisoo123', name: 'Hui Soo Chae', role: 'user' },
+  { email: 'hc3279@nyu.edu', password: 'Huisoo123', name: 'Hui Soo Chae', role: 'user' },
   { email: 'nsa258@nyu.edu', password: 'BlueCat258', name: 'Natasha Andaz', role: 'user' },
   { email: 'pb3263@nyu.edu', password: 'GreenFox3263', name: 'Patrick Brown Jr', role: 'user' },
   { email: 'elb8806@nyu.edu', password: 'WhiteLion8806', name: 'EL Burts', role: 'user' },
